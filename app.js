8143cfd9af97a13617319fedbf25397025331791
Daniel Hdz <dani.hernandez.guti@gmail.com>
2025-01-28 15:10:31 -0600

    Primer commit: Subida inicial del proyecto

---
+++ app.js
@@ -0,0 +1,55 @@
+let listaAmigos = [];
+
+
+function agregarAmigo() {
+    let textoNombre = document.getElementById('amigo').value;
+
+    if (textoNombre == '') {
+        alert('Por favor, inserte un nombre.');
+    } else {
+        listaAmigos.push(textoNombre);
+        actualizarLista();
+    }
+    limpiarCaja();
+}
+
+
+function limpiarCaja() {
+    document.querySelector('#amigo').value = '';
+}
+
+
+function actualizarLista() {
+    let listaHTML = document.getElementById('listaAmigos');
+    listaHTML.innerHTML = '';
+
+    for (let amigo of listaAmigos) {
+        let item = document.createElement('li');
+        item.textContent = amigo;
+        listaHTML.appendChild(item);
+    }
+}
+
+
+function sortearAmigo() {
+    if (listaAmigos.length === 0) {
+        alert('La lista está vacía. ¡Agrega amigos para poder jugar!');
+        return;
+    }
+
+    let indiceAmigo = Math.floor(Math.random() * listaAmigos.length);
+    let amigoSecreto = listaAmigos[indiceAmigo];
+
+    let resultadoAmigo = document.getElementById('resultado');
+    resultadoAmigo.innerHTML = `<li>🎉 Amigo sorteado: ${amigoSecreto}</li>`;
+
+    listaAmigos = [];
+    document.getElementById('listaAmigos').innerHTML = '';
+}
+
+function reiniciarJuego() {
+    listaAmigos = [];
+    document.getElementById('listaAmigos').innerHTML = '';
+    document.getElementById('resultado').innerHTML = '';
+    alert('El juego ha sido reiniciado. ¡Puedes empezar de nuevo!');
+}
